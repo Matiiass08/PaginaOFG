56519b66f1c927c37d26560f2c0775380935d976
matiiass08 <miretamales@uc.cl>
2024-06-23 20:33:46 -0400

    mejorando

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,10 @@
+#!/usr/bin/env bash
+# salir en caso de error
 set -o errexit
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+
 bundle exec rake db:migrate
 bundle exec rake db:seed
