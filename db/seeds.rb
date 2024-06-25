ab82eb166f70dc5767fd3aa595666565ac26d91c
matiiass08 <miretamales@uc.cl>
2024-06-24 23:11:50 -0400

    ultimos dos juegos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,87 +1,11 @@
-
 Juego.create(
-  nombre: "La Gallinita Ciega", 
-  descripcion: "Este juego se realiza en un espacio delimitado, relativamente pequeño y seguro. Uno o más jugadores son vendados para que no puedan ver, representando a las gallinitas ciegas, mientras los demás jugadores se colocan alrededor. Las gallinitas ciegas deben intentar atrapar a uno o más jugadores, quienes pueden moverse y emitir sonidos para despistar a las gallinitas ciegas. Una vez que la gallinita ciega atrapa a alguien, esa persona es sacada del campo de juego. El juego termina cuando todas las gallinitas ciegas han atrapado a todos los jugadores o cuando se acaba el tiempo.
-Objetivos: Fomentar la diversión y el entretenimiento, desarrollar habilidades sensoriales, especialmente el oído y el tacto, promover la agilidad y la rapidez de reacción."
-)
-
-Juego.create(
-  nombre: "Cachipun Evolución o Juego de la ameba", 
-  descripcion: "Es un juego para romper el hielo, donde en un terreno definido los participantes juegan cachipun (piedra, papel o tijera) entre ellos. A medida que transcurre el juego se debe imitar uno de los siguientes animales: ameba, cangrejo, conejo, gorila y humano. Todos comienzan siendo amebas. Al ganar una partida de cachipun evolucionas a cangrejo, luego a conejo y luego en gorila. Si ganas como gorila, evolucionas a humano y ganas el juego. Cabe destacar que solo pueden enfrentarse con personas que estén en el mismo nivel de evolución, es decir, amebas con amebas, cangrejos con cangrejos, conejos con conejos y gorilas con gorilas. Si en cualquier etapa de este juego pierdes, vuelves a ser ameba. Por lo tanto, para ganar se debe ganar 5 veces seguidas el cachipun. El juego termina cuando se llega a cierta cantidad de humanos o se acaba el tiempo.
-Objetivos: Romper el hielo entre gente que no se conocen, perder la timidez al simular ser cualquiera de estos animales, mejorar la interacción social y una competencia amistosa."
+  nombre: "Cruzando el río", 
+  descripcion: "Es un juego grupal para participantes de 7 años en adelante, diseñado para desarrollarse en una cancha de pasto sintético con un mínimo de 10 personas y sin límite máximo. Los jugadores se dividen en equipos y deben cruzar la cancha sin tocar el suelo con los pies, utilizando hojas de papel para pisar. Cada equipo se posiciona en la línea de partida y pasa una hoja extra desde el último jugador hasta el primero, avanzando uno a uno hasta cruzar la meta. Variantes incluyen realizar el recorrido de ida y vuelta, avanzando de espaldas, o usando más hojas para colocar cada pie en una hoja distinta.
+Objetivos: El juego fomenta la coordinación, el equilibrio, el trabajo en equipo y la comunicación."
 )
 
 Juego.create(
-  nombre: "Encontrar a tu grupo animal", 
-  descripcion: "Es un juego para romper el hielo en el que los participantes, con los ojos vendados u ojos cerrados, deben agruparse mediante sonidos de animales. Los jugadores se distribuyen por todo el terreno de juego y se les asigna un animal, como gato, perro, vaca, entre otros. Durante toda la partida, deben mantener los ojos cerrados o estar vendados. Para encontrar a los miembros de su grupo, solo pueden hacer el sonido característico de su animal (por ejemplo, 'miau' para el gato, 'guau' para el perro, 'muuu' para la vaca, etc.) y no pueden comunicarse de otra manera. El primer grupo en reunir a todos sus miembros gana el juego.
-Objetivos: Romper el hielo y fomentar la interacción entre los participantes, desarrollar habilidades de comunicación no verbal, promover el trabajo en equipo y la cooperación."
+  nombre: "El Teatro", 
+  descripcion: "El Teatro es un juego en el que participan al menos 10 jugadores, sin límite máximo de participantes. Se forman varios grupos, cada uno de los cuales escribe una obra de teatro que otro grupo deberá actuar. Tras recibir las obras de manera aleatoria, cada grupo reparte los roles que tomarán en la obra. Luego, cada equipo presenta su actuación frente a los demás y se califica en función de la fidelidad al guión recibido. El equipo con la mejor calificación en la actuación gana. El juego se desarrolla en etapas, y con antelación se decide el tiempo para escribir, organizarse y actuar.
+Objetivos: Fomentar la confianza y perder la vergüenza entre los participantes, promover un ambiente positivo y de buen compañerismo, facilitar la creatividad y la expresión artística, mejorar la coordinación y el trabajo en equipo."
 )
-
-
-Dinamica.create(
-  nombre: "El león", 
-  descripcion: "Es una dinámica simple donde el moderador hace un cántico que dice “yo tengo un león!!, vive en el zoológico!!, cola morena!!, y cuando rasguña, se deja una uña!!” Cada parte de la canción tiene una mímica asociada y el moderador tiene que decirlo con un tono alto y animado de voz para que los demás se animen a seguirlo."
-)
-
-Dinamica.create(
-  nombre: "El tren porfiado", 
-  descripcion: "Dinámica donde un líder canta una canción y realiza movimientos, mientras los demás participantes deben hacer y decir lo contrario. Por ejemplo, si el líder canta y gesticula 'para arriba,' los seguidores deben cantar y moverse 'para abajo.' La canción es:
-  'Yo tengo un tren que va para arriba (Bajar brazos), yo tengo un tren que va para arriba (Bajar brazos), para arriba, para arriba, para arriba (Bajar brazos), para arriba, para arriba, para abajo.(Bajar brazos y después levantarlos), yo tengo un tren que va para abajo (Levantar brazos), yo tengo un tren que va para abajo (Levantar brazos), Para abajo, para abajo, para abajo.(Levantar brazos), para abajo, para abajo, para arriba.'(Levantar brazos y después bajarlos)"
-)
-
-Dinamica.create(
-  nombre: "La canasta Tomate", 
-  descripcion: "Los participantes forman un círculo y colocan su mano derecha sobre la mano izquierda del compañero, creando una cadena de manos. Mientras cantan una canción, después de cada palabra, una persona da un aplauso con su mano derecha sobre la mano derecha de la persona a su izquierda, manteniendo la cadena en movimiento. La canción es: 'Toma, tomate, toma, lo, IA, IA, IO PLOP.' Cuando llegan al 'PLOP,' la persona que debería recibir el aplauso debe mover su mano para evitar ser tocada. Si es tocada, esa persona pierde y sale del círculo. El juego continúa hasta que solo quede una persona en el círculo."
-)
-
-Dinamica.create(
-  nombre: "La goma", 
-  descripcion: "Dinámica donde un líder canta una canción y realiza acciones que los demás deben seguir. Este líder simula tener una goma de cierto tamaño. La canción es: 'Yo quiero tener una goma así!!, que limpie mis dibujos!!, que borre mis cuadernos!!,Oooooooooooh la mansa goma!!!!' Después de cada repetición, la 'goma' imaginaria se hace más grande, y el último verso se canta más largo y con más énfasis, acompañando con gestos exagerados para representar el tamaño creciente de la goma."
-)
-
-Dinamica.create(
-  nombre: "El alacrán", 
-  descripcion: "Esta dinámica consiste en que los participantes se reúnen en un círculo y siguen a un líder que canta una canción y realiza gestos. Con cada verso, se agrega un gesto nuevo sobre cómo 'matar' al alacrán, y los participantes imitan estos movimientos. La canción es: 
-  'El alacrán, el alacrán
-  Como mueve su patita, sí señor
-  Se mata así, se mata así, así, así, así (con gestos)
-  El alacrán, el alacrán
-  Como mueve su colita, sí señor
-  Como mueve su patita, sí señor
-  Se mata así, se mata así, así, así, así (con gestos)
-  El alacrán, el alacrán
-  Como mueve su caderita, sí señor
-  Como mueve su colita, sí señor
-  Como mueve su patita, sí señor
-  Se mata así, se mata así, así, así, así (con gestos)'
-  Cada vez que se repite la canción, se añade un nuevo gesto, y todos deben recordar y realizar la secuencia completa de gestos."
-)
-
-Dinamica.create(
-  nombre: "Very One Two Three", 
-  descripcion: "Esta es una dinámica musical dirigida por un líder que canta una canción y guía los movimientos. Cada vez que se menciona una parte del cuerpo en la canción, los participantes deben tocarse esa parte, también se pueden nombrar ciertas acciones específicas dentro de la canción. Cuando se dice 'one, two, three,' deben chasquear los dedos 3 veces. La canción progresa añadiendo más partes del cuerpo y movimientos a la secuencia. La letra de la canción es:
-  'Cabeza, boca, very one, two, three
-  Mano, codo, very one, two, three
-  Cabeza, boca, mano, codo, very one, two, three
-  Pie, rodilla, very one, two, three
-  Cabeza, boca, mano, codo, pie, rodilla, very one, two, three
-  Medio paso, very one, two, three
-  Cabeza, boca, mano, codo, pie, rodilla, medio paso, very one, two, three
-  Paso entero, very one, two, three
-  Cabeza, boca, mano, codo, pie, rodilla, medio paso, paso entero, very one, two, three'"
-)
-Aplauso.create(
-  nombre: "Aplauso de la carrera de caballos", 
-  descripcion: "Es una carrera de caballos, todos los participantes deben estar dándose golpes en las piernas simulando un galope. El moderador narra la carrera con diferentes instrucciones en el orden que estime conveniente. Las instrucciones pueden ser: Pasa por el agua: pegarse en las mejillas, Pasa por el túnel: pegarse en el pecho, Pasa por el pasto: se frotan las manos, Gira a la derecha: se inclinan a la derecha, Gira a la izquierda: se inclinan a la izquierda, Salto/doble Salto: levantan los brazos, Llega a la meta: hacen una pose."
-)
-
-Aplauso.create(
-  nombre: "Aplauso popeye", 
-  descripcion: "Este aplauso imita la canción de 'Popeye' y utiliza las palmas y los muslos. Se canta la siguiente canción: Popeye el marino soy (palmas), (2 aplausos en los muslos), Comiendo espinaca voy (palmas), (2 aplausos en los muslos), Por los siete mares, conquisto a las chicas (palmas), Popeye el marino soy (palmas), (2 aplausos en los muslos). Después se intercambian las palmas por los aplausos en los muslos y vice versa."
-)
-
-Aplauso.create(
-  nombre: "Boom snap clap", 
-  descripcion: "Este aplauso tiene una canción y en cada parte hay que hacer una acción diferente. La canción dice así: Boom, snap, clap, Boom boom, snap, clap, snap, Boom, snap, clap, Boom boom, snap, Shh. En el boom hay que golpear con una mano en el pecho. En el snap hay que chasquear los dedos. En el clap hay que aplaudir y en el shh hay que hacer gesto de silencio con el dedo."
-)
-
