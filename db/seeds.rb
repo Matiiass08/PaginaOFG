84f2363af5c803f03986ba4da1c7881a9e48d26f
matiiass08 <miretamales@uc.cl>
2024-06-24 23:00:36 -0400

    ultimos juegos, dinamicas y aplausos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,87 @@
 
-# Creación del juego "Policías y ladrones"
 Juego.create(
-  nombre: "Policías y ladrones", 
-  descripcion: "Este juego es un clásico juego de persecución en el que los jugadores se dividen en dos grupos: policías y ladrones. Este juego necesita de un espacio grande donde se pueda correr, como una cancha. Los ladrones deben correr y esconderse, mientras que los policías intentan atraparlos. Si un ladrón es tocado por un policía, cuenta como atrapado y este tendrá que dejarse llevar hacia una cárcel designada. Los ladrones tendrán una oportunidad para liberar a sus compañeros tocando una pared o cosa específica, donde al hacerlo, se liberan a todos los ladrones dentro de la cárcel. El juego continúa hasta que todos los ladrones hayan sido atrapados o hasta que se acabe el tiempo. Objetivos: Fomentar el trabajo en equipo entre los policías para coordinar la captura de los ladrones, desarrollar la agilidad y la resistencia física al correr y esquivar de los ladrones y mejorar la estrategia y la toma de decisiones rápidas tanto para los policías como para los ladrones."
+  nombre: "La Gallinita Ciega", 
+  descripcion: "Este juego se realiza en un espacio delimitado, relativamente pequeño y seguro. Uno o más jugadores son vendados para que no puedan ver, representando a las gallinitas ciegas, mientras los demás jugadores se colocan alrededor. Las gallinitas ciegas deben intentar atrapar a uno o más jugadores, quienes pueden moverse y emitir sonidos para despistar a las gallinitas ciegas. Una vez que la gallinita ciega atrapa a alguien, esa persona es sacada del campo de juego. El juego termina cuando todas las gallinitas ciegas han atrapado a todos los jugadores o cuando se acaba el tiempo.
+Objetivos: Fomentar la diversión y el entretenimiento, desarrollar habilidades sensoriales, especialmente el oído y el tacto, promover la agilidad y la rapidez de reacción."
 )
 
-# Creación del juego "La orden del Vikingo"
 Juego.create(
-  nombre: "La orden del Vikingo", 
-  descripcion: "Los jugadores forman un círculo y se les asigna un número en orden, de tal manera que cada jugador tenga a su antecesor numérico a un lado y a su sucesor numérico al otro lado. Exceptuando la persona con el número 1, cuyo antecesor será la persona con el último número, y la persona con el último número tendrá a la persona con el número 1 como antecesor. El juego comienza con alguien tomando el rol de líder vikingo, simulando tener un casco con cuernos usando sus manos. Las personas a su lado derecho e izquierdo deben simular remar hacia el lado correspondiente (derecho si están a su derecha y viceversa). Después de remar, el líder vikingo designa a otro líder aleatoriamente diciendo, por ejemplo, 'ahora el líder vikingo es el 6'. Entonces, la persona con ese número simula el casco y las personas a sus lados empiezan a remar. Cada vez que alguien se equivoca en el movimiento (simulando el casco o remando en la dirección incorrecta), las tres personas involucradas pierden y salen del círculo, que se hace más pequeño. El juego termina cuando queden tres personas o se acabe el tiempo. Objetivos: Fomentar la atención y concentración para seguir correctamente las instrucciones y movimientos, desarrollar la coordinación y la capacidad de respuesta rápida a los cambios de líder, promover la interacción y el trabajo en equipo, ya que todos deben estar atentos a las acciones de los demás, mejorar la memoria y la secuencia numérica al recordar los números y las posiciones de los otros jugadores."
+  nombre: "Cachipun Evolución o Juego de la ameba", 
+  descripcion: "Es un juego para romper el hielo, donde en un terreno definido los participantes juegan cachipun (piedra, papel o tijera) entre ellos. A medida que transcurre el juego se debe imitar uno de los siguientes animales: ameba, cangrejo, conejo, gorila y humano. Todos comienzan siendo amebas. Al ganar una partida de cachipun evolucionas a cangrejo, luego a conejo y luego en gorila. Si ganas como gorila, evolucionas a humano y ganas el juego. Cabe destacar que solo pueden enfrentarse con personas que estén en el mismo nivel de evolución, es decir, amebas con amebas, cangrejos con cangrejos, conejos con conejos y gorilas con gorilas. Si en cualquier etapa de este juego pierdes, vuelves a ser ameba. Por lo tanto, para ganar se debe ganar 5 veces seguidas el cachipun. El juego termina cuando se llega a cierta cantidad de humanos o se acaba el tiempo.
+Objetivos: Romper el hielo entre gente que no se conocen, perder la timidez al simular ser cualquiera de estos animales, mejorar la interacción social y una competencia amistosa."
 )
 
-# Creación del juego "Cachipún Alemán"
 Juego.create(
-  nombre: "Cachipún Alemán", 
-  descripcion: "Consiste en una versión grupal y dinámica del clásico juego de piedra, papel o tijera (también conocido como 'cachipún'). Los jugadores forman dos hileras, situadas a cierta distancia una de otra. Una persona de cada hilera tendrá que hacer un recorrido en forma invertida de letra 'U' (en cada punta hay una hilera), de tal manera que se topen con el oponente. Cuando se encuentran tienen que jugar una ronda de cachipún (piedra, papel o tijera). El jugador que gana la ronda continúa avanzando hacia la línea de partida del equipo contrario, mientras que el jugador que pierde regresa al final de su fila, y el siguiente jugador de su equipo avanza para encontrarse con el ganador anterior. El objetivo es que un jugador logre llegar a la línea de partida del equipo contrario para ganar. Objetivos: Fomentar la rapidez mental y la toma de decisiones rápidas, promover el trabajo en equipo y la coordinación, ya que los jugadores deben estar listos para enfrentarse rápidamente y adaptarse a las situaciones, mejorar la interacción social y la competencia amistosa entre los participantes, desarrollar habilidades motoras y de movimiento, ya que los jugadores deben avanzar rápidamente hacia el centro y luego hacia la línea de meta."
-)
-
-# Creación del juego "Perro, Gato y Ratón"
-Juego.create(
-  nombre: "Perro, Gato y Ratón", 
-  descripcion: "Los participantes se dividen en tres grupos con igual cantidad de personas, formando círculos tomados de las manos y mirando hacia el centro. Un grupo representa a los ratones, otro a los perros y otro a los gatos. Cada grupo elige un representante que se coloca en el centro del círculo. La meta del juego es que los representantes salgan del círculo, persigan y toquen a otros jugadores dependiendo del animal que representan, el perro persigue al gato, el gato al ratón y el ratón al perro. El primero en tocar a su objetivo gana la ronda. Los representantes no pueden ser atrapados mientras estén dentro de su círculo, pero tienen un límite de tiempo para permanecer allí. El representante que es tocado y que determina el término de la ronda, se convierte en parte del círculo del representante que lo tocó. Objetivos: Fomentar la rapidez y agilidad física, promover el trabajo en equipo y la coordinación grupal, desarrollar habilidades de estrategia y planificación, mejorar la interacción social y la competencia amistosa."
+  nombre: "Encontrar a tu grupo animal", 
+  descripcion: "Es un juego para romper el hielo en el que los participantes, con los ojos vendados u ojos cerrados, deben agruparse mediante sonidos de animales. Los jugadores se distribuyen por todo el terreno de juego y se les asigna un animal, como gato, perro, vaca, entre otros. Durante toda la partida, deben mantener los ojos cerrados o estar vendados. Para encontrar a los miembros de su grupo, solo pueden hacer el sonido característico de su animal (por ejemplo, 'miau' para el gato, 'guau' para el perro, 'muuu' para la vaca, etc.) y no pueden comunicarse de otra manera. El primer grupo en reunir a todos sus miembros gana el juego.
+Objetivos: Romper el hielo y fomentar la interacción entre los participantes, desarrollar habilidades de comunicación no verbal, promover el trabajo en equipo y la cooperación."
 )
 
 
-Aplauso.create(nombre: "Aplauso de la carrera de caballos", descripcion: "Es una carrera de caballos, todos los participantes deben estar dándose golpes en las piernas simulando un galope. El moderador narra la carrera con diferentes instrucciones en el orden que estime conveniente. Las instrucciones pueden ser: Pasa por el agua: pegarse en las mejillas, Pasa por el túnel: pegarse en el pecho, Pasa por el pasto: se frotan las manos, Gira a la derecha: se inclinan a la derecha, Gira a la izquierda: se inclinan a la izquierda, Salto/doble Salto: levantan los brazos, Llega a la meta: hacen una pose.")
+Dinamica.create(
+  nombre: "El león", 
+  descripcion: "Es una dinámica simple donde el moderador hace un cántico que dice “yo tengo un león!!, vive en el zoológico!!, cola morena!!, y cuando rasguña, se deja una uña!!” Cada parte de la canción tiene una mímica asociada y el moderador tiene que decirlo con un tono alto y animado de voz para que los demás se animen a seguirlo."
+)
 
-Aplauso.create(nombre: "Aplauso popeye", descripcion: "Este aplauso imita la canción de 'Popeye' y utiliza las palmas y los muslos. Se canta la siguiente canción: Popeye el marino soy (palmas), (2 aplausos en los muslos), Comiendo espinaca voy (palmas), (2 aplausos en los muslos), Por los siete mares, conquisto a las chicas (palmas), Popeye el marino soy (palmas), (2 aplausos en los muslos). Después se intercambian las palmas por los aplausos en los muslos y vice versa.")
+Dinamica.create(
+  nombre: "El tren porfiado", 
+  descripcion: "Dinámica donde un líder canta una canción y realiza movimientos, mientras los demás participantes deben hacer y decir lo contrario. Por ejemplo, si el líder canta y gesticula 'para arriba,' los seguidores deben cantar y moverse 'para abajo.' La canción es:
+  'Yo tengo un tren que va para arriba (Bajar brazos), yo tengo un tren que va para arriba (Bajar brazos), para arriba, para arriba, para arriba (Bajar brazos), para arriba, para arriba, para abajo.(Bajar brazos y después levantarlos), yo tengo un tren que va para abajo (Levantar brazos), yo tengo un tren que va para abajo (Levantar brazos), Para abajo, para abajo, para abajo.(Levantar brazos), para abajo, para abajo, para arriba.'(Levantar brazos y después bajarlos)"
+)
 
-Aplauso.create(nombre: "Boom snap clap", descripcion: "Este aplauso tiene una canción y en cada parte hay que hacer una acción diferente. La canción dice así: Boom, snap, clap, Boom boom, snap, clap, snap, Boom, snap, clap, Boom boom, snap, Shh. En el boom hay que golpear con una mano en el pecho. En el snap hay que chasquear los dedos. En el clap hay que aplaudir y en el shh hay que hacer gesto de silencio con el dedo.")
+Dinamica.create(
+  nombre: "La canasta Tomate", 
+  descripcion: "Los participantes forman un círculo y colocan su mano derecha sobre la mano izquierda del compañero, creando una cadena de manos. Mientras cantan una canción, después de cada palabra, una persona da un aplauso con su mano derecha sobre la mano derecha de la persona a su izquierda, manteniendo la cadena en movimiento. La canción es: 'Toma, tomate, toma, lo, IA, IA, IO PLOP.' Cuando llegan al 'PLOP,' la persona que debería recibir el aplauso debe mover su mano para evitar ser tocada. Si es tocada, esa persona pierde y sale del círculo. El juego continúa hasta que solo quede una persona en el círculo."
+)
 
+Dinamica.create(
+  nombre: "La goma", 
+  descripcion: "Dinámica donde un líder canta una canción y realiza acciones que los demás deben seguir. Este líder simula tener una goma de cierto tamaño. La canción es: 'Yo quiero tener una goma así!!, que limpie mis dibujos!!, que borre mis cuadernos!!,Oooooooooooh la mansa goma!!!!' Después de cada repetición, la 'goma' imaginaria se hace más grande, y el último verso se canta más largo y con más énfasis, acompañando con gestos exagerados para representar el tamaño creciente de la goma."
+)
 
+Dinamica.create(
+  nombre: "El alacrán", 
+  descripcion: "Esta dinámica consiste en que los participantes se reúnen en un círculo y siguen a un líder que canta una canción y realiza gestos. Con cada verso, se agrega un gesto nuevo sobre cómo 'matar' al alacrán, y los participantes imitan estos movimientos. La canción es: 
+  'El alacrán, el alacrán
+  Como mueve su patita, sí señor
+  Se mata así, se mata así, así, así, así (con gestos)
+  El alacrán, el alacrán
+  Como mueve su colita, sí señor
+  Como mueve su patita, sí señor
+  Se mata así, se mata así, así, así, así (con gestos)
+  El alacrán, el alacrán
+  Como mueve su caderita, sí señor
+  Como mueve su colita, sí señor
+  Como mueve su patita, sí señor
+  Se mata así, se mata así, así, así, así (con gestos)'
+  Cada vez que se repite la canción, se añade un nuevo gesto, y todos deben recordar y realizar la secuencia completa de gestos."
+)
 
-Dinamica.create(nombre: "La dinámica de la pata", descripcion: "Todos están en círculo y en hilera, cada vez que dicen 'pachi' se avanza un paso con uno de tus pies. La canción es la siguiente: Ahí viene mamá pata (¡Pachi!), Ahí viene papá pato (¡Pachi!), Ahí vienen los patitos (¡Pachi! ¡Pachi! ¡Pachi!), Ten cuidado Pachí, con los patitos no te metai.")
+Dinamica.create(
+  nombre: "Very One Two Three", 
+  descripcion: "Esta es una dinámica musical dirigida por un líder que canta una canción y guía los movimientos. Cada vez que se menciona una parte del cuerpo en la canción, los participantes deben tocarse esa parte, también se pueden nombrar ciertas acciones específicas dentro de la canción. Cuando se dice 'one, two, three,' deben chasquear los dedos 3 veces. La canción progresa añadiendo más partes del cuerpo y movimientos a la secuencia. La letra de la canción es:
+  'Cabeza, boca, very one, two, three
+  Mano, codo, very one, two, three
+  Cabeza, boca, mano, codo, very one, two, three
+  Pie, rodilla, very one, two, three
+  Cabeza, boca, mano, codo, pie, rodilla, very one, two, three
+  Medio paso, very one, two, three
+  Cabeza, boca, mano, codo, pie, rodilla, medio paso, very one, two, three
+  Paso entero, very one, two, three
+  Cabeza, boca, mano, codo, pie, rodilla, medio paso, paso entero, very one, two, three'"
+)
+Aplauso.create(
+  nombre: "Aplauso de la carrera de caballos", 
+  descripcion: "Es una carrera de caballos, todos los participantes deben estar dándose golpes en las piernas simulando un galope. El moderador narra la carrera con diferentes instrucciones en el orden que estime conveniente. Las instrucciones pueden ser: Pasa por el agua: pegarse en las mejillas, Pasa por el túnel: pegarse en el pecho, Pasa por el pasto: se frotan las manos, Gira a la derecha: se inclinan a la derecha, Gira a la izquierda: se inclinan a la izquierda, Salto/doble Salto: levantan los brazos, Llega a la meta: hacen una pose."
+)
+
+Aplauso.create(
+  nombre: "Aplauso popeye", 
+  descripcion: "Este aplauso imita la canción de 'Popeye' y utiliza las palmas y los muslos. Se canta la siguiente canción: Popeye el marino soy (palmas), (2 aplausos en los muslos), Comiendo espinaca voy (palmas), (2 aplausos en los muslos), Por los siete mares, conquisto a las chicas (palmas), Popeye el marino soy (palmas), (2 aplausos en los muslos). Después se intercambian las palmas por los aplausos en los muslos y vice versa."
+)
+
+Aplauso.create(
+  nombre: "Boom snap clap", 
+  descripcion: "Este aplauso tiene una canción y en cada parte hay que hacer una acción diferente. La canción dice así: Boom, snap, clap, Boom boom, snap, clap, snap, Boom, snap, clap, Boom boom, snap, Shh. En el boom hay que golpear con una mano en el pecho. En el snap hay que chasquear los dedos. En el clap hay que aplaudir y en el shh hay que hacer gesto de silencio con el dedo."
+)
+
